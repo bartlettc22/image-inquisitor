5329e830e89af805af40c0a4f926503698230818
Chris Bartlett <bartlettc@gmail.com>
2025-08-23 18:20:30 -0600

    chart and mirroring

---
+++ scripts/publish_helm_charts.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for chart in charts/*/; do
+if [ -d "${chart}" ]; then
+    chart_name=$(basename "${chart}")
+    
+    # Update dependencies
+    helm dependency update "${chart}"
+    
+    # Package chart
+    helm package "${chart}"
+    
+    # Push to OCI registry
+    helm push "${chart_name}"-*.tgz oci://ghcr.io/bartlettc22
+    
+    # Clean up
+	rm "${chart_name}"-*.tgz
+fi
+done
